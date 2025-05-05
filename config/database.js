8e650023475af46edf4e7114660db2bd9c174c28
immonroe <immanuelmonroe@gmail.com>
2025-05-05 11:47:05 -0400

    Updated styling for all pages

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 // config/database.js
 module.exports = {
     'url' : 'mongodb+srv://finance:finance@cluster0.mugppq1.mongodb.net/demo?retryWrites=true&w=majority&appName=Cluster0',
-
     'dbName': 'demo'
 };
